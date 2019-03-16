07e6189219d98208a9b0f570494eab8eee685421
Vanya Levchenko <ivanlevchenko56@gmail.com>
2019-03-16 14:52:58 -0600

    create-react-app and src scaffolding created.

---
+++ build/precache-manifest.d0dda09eb65f306b8cd3b0872c1e9b90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4456d2a341aa42c2e410",
+    "url": "/static/js/main.4456d2a3.chunk.js"
+  },
+  {
+    "revision": "a14d8c4a0cb4b6d36e52",
+    "url": "/static/js/2.a14d8c4a.chunk.js"
+  },
+  {
+    "revision": "83584d241b1bcbdc0f068290ca3de2ac",
+    "url": "/index.html"
+  }
+];
---
+++ build/static/js/main.4456d2a3.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},9:function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o),a=t(2);var c=function(){return r.a.createElement("p",null,"Render Bootstrap components here")};t.n(a).a.render(r.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.4456d2a3.chunk.js.map
