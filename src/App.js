e7998c4297314ee957e6bab53419fe1b1f125abd
Vanya Levchenko <ivanlevchenko56@gmail.com>
2019-03-18 13:13:55 -0600

    Image cards and app.js files finnished.

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     this.state.cards.forEach(card => {
       card.count = 0;
     });
-    alert(`Game Over :( \nscore: ${this.state.score}`);
+    alert(`GAME OVER!! \nYou chose....poorly \nscore: ${this.state.score}`);
     this.setState({score: 0});
     return true;
   }
